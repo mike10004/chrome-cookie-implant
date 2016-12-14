9b5902687489c97e1a00c6db53113f1fb86b7ebb
Mike Chaberski <mchaberski@novetta.com>
2016-12-14 13:41:50 -0500

    include version in output file

--- make.sh
+++ make.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
 CHROME=$(which google-chrome)
+if [ -z "$CHROME" ] ; then
+  echo "make.sh: google-chrome is not installed" >&2
+  exit 1
+fi
 
 fail() {
     echo $1 >&2
     exit $2 
 }
 
-KEY_OPT=""
-rm -rf src.crx || fail "failed to remove existing .crx" $?
+INPUT="src"
+OUTPUT="chrome-cookie-implant"
 
-KEY_FILE="${PWD}/src.pem"
+KEY_OPT=""
+rm -rf "./${INPUT}.crx" || fail "failed to remove existing .crx" $?
+
+KEY_FILE="${PWD}/${INPUT}.pem"
 
 if [ -f "${KEY_FILE}" ] ; then
-    echo "using existing key ${KEY_FILE}"
     KEY_OPT="--pack-extension-key=${KEY_FILE}"
+else
+    echo "make.sh: no existing key; expected at ${KEY_FILE}" >&2
 fi
 
-$CHROME --pack-extension=${PWD}/src "${KEY_OPT}" || fail "failed to pack" $?
+$CHROME --pack-extension=${PWD}/${INPUT} "${KEY_OPT}" 2>/dev/null || fail "failed to pack" $?
 
-ls src.*
+TEMPFILE="${PWD}/${INPUT}.crx"
 
-OUTFILE="${PWD}/src.crx"
-EXT_ID=$(python crxmetadata.py --extension-id "$OUTFILE")
+OUTNAME="${OUTPUT}"
+VERSION=$(python -c 'import sys, json; print json.load(sys.stdin)["version"]' < "${INPUT}/manifest.json")
+if [ -n "$VERSION" ] ; then
+  OUTNAME="${OUTNAME}-${VERSION}"
+fi
+OUTNAME="${OUTNAME}.crx"
+mv "$TEMPFILE" "$OUTNAME"
+EXT_ID=$(python crxmetadata.py --extension-id "$OUTNAME")
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
   fail "crx metadata parse failure" $?
 fi
-echo "extension id: $EXT_ID"
+echo "${OUTNAME}: $EXT_ID"
