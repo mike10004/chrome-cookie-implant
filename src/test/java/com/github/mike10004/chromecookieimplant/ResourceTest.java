c7059dc128aa6d42fb33eb9589845aaa21b27f1f
Mike Chaberski <mchaberski@gmail.com>
2020-02-06 15:38:06 -0500

    start 1.5.16: use updated crxtool to produce crx version 3

--- src/test/java/com/github/mike10004/chromecookieimplant/ResourceTest.java
+++ src/test/java/com/github/mike10004/chromecookieimplant/ResourceTest.java
@@ -1,9 +1,16 @@
 package com.github.mike10004.chromecookieimplant;
 
+import io.github.mike10004.crxtool.CrxInventory;
+import io.github.mike10004.crxtool.CrxMetadata;
+import io.github.mike10004.crxtool.CrxParser;
+import io.github.mike10004.crxtool.CrxVersion;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.net.URL;
 
+import static com.google.common.base.Preconditions.checkState;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ResourceTest {
@@ -12,6 +19,18 @@ public class ResourceTest {
     public void testPluginBuiltExtensionInClasspath() throws Exception {
         URL resource = getClass().getResource("/chrome-cookie-implant.crx");
         assertNotNull("resource not in test output dir", resource);
+    }
+
+    @Test
+    public void testExtensionIsCrxVersion3() throws Exception {
+        URL resource = getClass().getResource("/chrome-cookie-implant.crx");
+        checkState(resource != null, "resource not found");
+        CrxInventory inventory;
+        try (InputStream in = resource.openStream()) {
+            inventory = CrxParser.getDefault().parseInventory(in);
+        }
+        CrxMetadata metadata = inventory.metadata();
+        assertEquals("metadata says version 3", CrxVersion.CRX3, metadata.getCrxVersion());
     }
 
 }
